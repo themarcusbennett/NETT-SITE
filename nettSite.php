443b3c89def1ff0ec26b231d11d783fc4be217ea
Marcus Bennett <marcus@minimalmar.com>
2019-01-15 13:49:05 -0500

    first commit

---
+++ nettSite.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<link rel="stylesheet" type="text/css" href="nettsite.css">
+	<title>NETT SITE</title>
+</head>
+<body>
+<?php 
+echo "Hello";
+?>
+
+</body>
+</html>
