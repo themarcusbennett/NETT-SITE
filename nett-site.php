d8bca7d87846f1246b8fdb180a7f04db1ac22e88
Marcus Bennett <marcus@minimalmar.com>
2019-01-18 01:14:17 -0500

    added scripts and bootstrap

--- nett-site.php
+++ nett-site.php
@@ -1,13 +1,31 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
-	<link rel="stylesheet" type="text/css" href="nettsite.css">
-	<title>NETT SITE</title>
+	<title>NETT-SITE</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<link rel="stylesheet" type="text/css" href="nett-site.css">
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
 </head>
 <body>
+
 <?php 
-echo "Hello";
+
 ?>
+
+	<h1>NETT-SITE</h1>
+	<p>Consulting</p>	
+	<p>Lorem Ipsum Dolor</p>
+	<p>Lorem Ipsum Dolor</p>
+	<p>Lorem Ipsum Dolor</p>
+
+<section>
+	<!-- BOOTSTRAP -->	
+	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js" integrity="sha384-B0UglyR+jN6CkvvICOB2joaf5I4l3gm9GU6Hc1og6Ls7i6U/mkkaduKaBhlAXv9k" crossorigin="anonymous"></script>
+	<!-- JQUERY -->
+	<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+	<!-- POPPER -->
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js" integrity="sha384-wHAiFfRlMFy6i5SRaxvfOCifBUQy1xHdJ/yoi7FRNXMRBu5WHdZYu1hA6ZOblgut" crossorigin="anonymous"></script>
+</section>
 
 </body>
 </html>
