ebd147ef727634810901d197d95274b45394c982
Marcus Bennett <marcus@minimalmar.com>
2019-02-04 23:56:12 -0500

    didnt add much

--- nett-site.php
+++ nett-site.php
@@ -12,11 +12,21 @@
 
 ?>
 
-	<h1>NETT-SITE</h1>
-	<p>Consulting</p>	
-	<p>Lorem Ipsum Dolor</p>
-	<p>Lorem Ipsum Dolor</p>
-	<p>Lorem Ipsum Dolor</p>
+<nav class="navbar mr navbar-expand-lg navbar-light bg-light">
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+    <div class="navbar-nav ml-auto">
+      <a class="nav-item nav-link active" href="#">Home <span class="sr-only">(current)</span></a>
+      <a class="nav-item nav-link" href="#">Pricing</a>
+      <a class="nav-item nav-link disabled" href="#" tabindex="-1" aria-disabled="true">123-456-7890</a>
+    </div>
+  </div>
+</nav>
+
+	<h1 class="title__name">NETT-SITE</h1>
+	<p>Consulting</br>Lorem Ipsum Dolor</br>Lorem Ipsum Dolor</br>Lorem Ipsum Dolor</p>
 
 <section>
 	<!-- BOOTSTRAP -->	
